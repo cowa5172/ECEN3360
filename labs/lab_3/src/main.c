7c7c8acdc0c9564d0641a3974b1d91f28bc39152
dmwo <37816602+dmwo@users.noreply.github.com>
2019-02-05 14:02:41 -0700

    initial lab 3 commit

---
+++ labs/lab_3/src/main.c
@@ -0,0 +1,72 @@
+/***************************************************************************//**
+ * @file
+ * @brief Simple LED Blink Demo for SLSTK3402A
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "bsp.h"
+#include "main.h"
+#include "gpio.h"
+#include "cmu.h"
+#include "letimer.h"
+
+
+int main(void)
+{
+  /* initialized variables */
+  Sleep_Block_Mode(EM4); 	// set lowest energy mode for the system
+
+  EMU_DCDCInit_TypeDef dcdcInit = EMU_DCDCINIT_DEFAULT;
+  CMU_HFXOInit_TypeDef hfxoInit = CMU_HFXOINIT_DEFAULT;
+
+  /* Chip errata */
+  CHIP_Init();
+
+  /* Init DCDC regulator and HFXO with kit specific parameters */
+  /* Initialize DCDC. Always start in low-noise mode. */
+  EMU_EM23Init_TypeDef em23Init = EMU_EM23INIT_DEFAULT;
+  EMU_DCDCInit(&dcdcInit);
+  em23Init.vScaleEM23Voltage = emuVScaleEM23_LowPower;
+  EMU_EM23Init(&em23Init);
+  CMU_HFXOInit(&hfxoInit);
+
+  /* Switch HFCLK to HFXO and disable HFRCO */
+  CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+  CMU_OscillatorEnable(cmuOsc_HFRCO, false, false);
+
+  /* Initialize clocks */
+  cmu_init();
+
+  /* Initialize GPIO */
+  gpio_init();
+
+  /* Initialize LETIMER0 */
+  letimer0_init();
+
+  LETIMER_Enable(LETIMER0, true);
+
+  // enable interrupts after chip has been configured
+  CORE_ATOMIC_IRQ_ENABLE();
+
+
+  while (1) {
+	  Enter_Sleep();
+  }
+}
