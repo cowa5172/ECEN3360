f54a5726f08262da4c6b2ae3100ded5b0640600a
dmwo <37816602+dmwo@users.noreply.github.com>
2019-02-07 13:41:51 -0700

    added documentation

--- labs/lab_3/src/i2c.c
+++ labs/lab_3/src/i2c.c
@@ -9,5 +9,12 @@ void i2c0_init(void){
     i2c_init.freq    = SI7021_FREQ,      /* Set to standard rate assuring being */
     i2c_init.clhr    = i2cClockHLRFast   /* Set to use 4:4 low/high duty cycle. */
 
-    I2C_Init(I2C0, &i2c_init)
+    I2C_Init(I2C0, &i2c_init);
 }
+
+// write to CMD to start i2c communication
+// write 0x40 to TX, (0x40 << 1) | 0
+// while wait for ack
+// write 0xE7 to read RH/T user register
+// send repeated start
+// write (0x40 << 1) | 1 to read
