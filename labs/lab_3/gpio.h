5f6a03f79a1407e5d629a87b9d903ea46565e226
dmwo <37816602+dmwo@users.noreply.github.com>
2019-02-13 16:52:56 -0700

    cosmetic changes

--- labs/lab_3/gpio.h
+++ labs/lab_3/gpio.h
@@ -2,28 +2,28 @@
 #include "em_gpio.h"
 
 /******************************************************************************
- * filename: gpio.h 														  *
- * 																			  *
+ * filename: gpio.h                                                           *
+ *                                                                            *
  * purpose: contains function declarations and macros used in gpio.c          *
- * 																			  *
- * date created: 22 Jan 2019												  *
- *																			  *
- * authors: Dylan Oh and Mike Fruge											  *
+ *                                                                            *
+ * date created: 22 Jan 2019                                                  *
+ *                                                                            *
+ * authors: Dylan Oh and Mike Fruge                                           *
  *****************************************************************************/
 
 /******************************************************************************
- * MACRO DEFINITIONS 	    				 								  *
+ * MACRO DEFINITIONS                                                          *
  *****************************************************************************/
 
 /* LED0 properties */
-#define	LED0_PORT		gpioPortF
+#define	LED0_PORT       gpioPortF
 #define LED0_PIN        4
-#define LED0_DEFAULT	false
+#define LED0_DEFAULT	  false
 
 /* LED1 properties */
 #define LED1_PORT       gpioPortF
 #define LED1_PIN        5
-#define LED1_DEFAULT	false
+#define LED1_DEFAULT    false
 
 /* SI7021 clock and data properties */
 #define I2C_SCL_PORT    gpioPortC
@@ -40,6 +40,6 @@
 
 
 /******************************************************************************
- * FUNCTION DECLARATIONS  					 								  *
+ * FUNCTION DECLARATIONS                                                      *
  *****************************************************************************/
 void gpio_init(void);
