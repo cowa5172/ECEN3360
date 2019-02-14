ec4468ba1129cc8e6db0ea67b567ffb398565403
dmwo <37816602+dmwo@users.noreply.github.com>
2019-02-13 17:03:49 -0700

    cosmetic changes

--- labs/lab_3/main.h
+++ labs/lab_3/main.h
@@ -2,17 +2,17 @@
 #include <stdbool.h>
 
 /******************************************************************************
- * filename: main.h				    										  *
- * 																			  *
+ * filename: main.h                                                           *
+ *                                                                            *
  * purpose: contains function declarations and macros used in main.c as well  *
  *          as changeable macros to be used by the instruction team to        *
  *          evaluate the code                                                 *
- * 																			  *
- * date created: 22 Jan 2019												  *
- *																			  *
- * authors: Dylan Oh and Mike Fruge											  *
+ *                                                                            *
+ * date created: 22 Jan 2019                                                  *
+ *                                                                            *
+ * authors: Dylan Oh and Mike Fruge                                           *
  *****************************************************************************/
 
 /******************************************************************************
- * MACRO DEFINITIONS     					 								  *
+ * MACRO DEFINITIONS                                                          *
  *****************************************************************************/
