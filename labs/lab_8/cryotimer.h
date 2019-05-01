c802667f24e260c6e75fe5b610515793d78aab68
dmwo <37816602+dmwo@users.noreply.github.com>
2019-04-30 20:46:01 -0600

    fixing cryotimer

--- labs/lab_8/cryotimer.h
+++ labs/lab_8/cryotimer.h
@@ -1,4 +1,14 @@
 #include "em_cryotimer.h"
 
+/******************************************************************************
+ * filename: cryotimer.c                                                      *
+ *                                                                            *
+ * purpose:
+ *                                                                            *
+ * date created: 23 April 2019                                                *
+ *                                                                            *
+ * authors: Dylan Oh and Mike Fruge                                           *
+ *****************************************************************************/
+
 void cryotimer_setup(void);
 void CRYOTIMER_IRQHandler(void);
